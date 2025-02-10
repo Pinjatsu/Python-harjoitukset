ba9ebde0ba1897f89b9f38e083e5c6c5914c3a6d
Pinjatsu <piayli@utu.fi>
2025-02-10 21:40:34 +0200

    Kaks ekaa tehty :3

---
+++ Currency exchange.py
@@ -0,0 +1,13 @@
+def exchange_money(budget, exchange_rate):
+    print("Estimate value after exchange: ",budget/exchange_rate)
+
+exchange_money(127.5,1.2)
+
+def get_change(budget, exchanging_value):
+    print("Calculate currency left after an exchange: ", budget-exchanging_value)
+
+get_change(127.5, 120)
+
+def get_value_of_bills():
+    print()
+    
